e5e01f5a1438d2ab2cb7d8362cf270d155dbcb9d
Samuel Ajibade <samuelajibade22@gmail.com>
2023-10-24 12:36:42 +0100

    added api endpoints for transactions

--- budpay/src/main/java/com/ajibsbaba/android/api/service/ApiService.kt
+++ budpay/src/main/java/com/ajibsbaba/android/api/service/ApiService.kt
@@ -1,4 +1,23 @@
 package com.ajibsbaba.android.api.service
 
-interface ApiService {
+import com.ajibsbaba.android.api.model.TransactionApiResponse
+import retrofit2.Call
+import retrofit2.http.FieldMap
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+public interface ApiService {
+
+    @GET("/transaction/verify/{reference}")
+    fun verifyTransaction(@Path("reference") reference: String) : Call<TransactionApiResponse>
+
+    @GET("/transaction/{id}")
+    fun fetchTransaction(@Path("transaction/{id}") id: String) : Call<TransactionApiResponse>
+
+    @FormUrlEncoded
+    @POST("/banktransfer/initialize")
+    fun bankTransfer(@FieldMap fields: HashMap<String, String>) : Call<TransactionApiResponse>
+
 }
