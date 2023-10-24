45dfcbf64fa86c1afb7449a54df15661f896d053
Samuel Ajibade <samuelajibade22@gmail.com>
2023-10-24 12:32:18 +0100

    created data classes for transaction response

--- budpay/src/main/java/com/ajibsbaba/android/api/model/TransactionApiResponse.kt
+++ budpay/src/main/java/com/ajibsbaba/android/api/model/TransactionApiResponse.kt
@@ -1,4 +1,32 @@
 package com.ajibsbaba.android.api.model
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
-class TransactionApiResponse {
+
+@JsonClass(generateAdapter = true)
+data class TransactionApiResponse(
+    @Json(name = "amount")
+    val amount: String? = null,
+
+    @Json(name = "currency")
+    val currency: String? = null,
+
+    @Json(name = "status")
+    val status: String? = null,
+
+    @Json(name = "transaction_date")
+    val transactionDate: String? = null,
+
+    @Json(name = "reference")
+    val reference: String? = null,
+
+    @Json(name = "domain")
+    val domain: String? = null,
+
+    @Json(name = "fees")
+    val fees: String? = null
+) {
+    fun isReferenceValid(): Boolean {
+        return reference != null
+    }
 }
--- budpay/src/main/java/com/ajibsbaba/android/model/Transaction.kt
+++ budpay/src/main/java/com/ajibsbaba/android/model/Transaction.kt
@@ -1,4 +1,60 @@
 package com.ajibsbaba.android.model
 
+import com.google.gson.annotations.SerializedName
+
+
 class Transaction {
+    val amount: String? = null
+    val currency: String? = null
+    val status: String? = null
+    val transactionDate: String? = null
+    val reference: String? = null
+    val domain: String? = null
+    val fees: String? = null
 }
+
+data class TransactionResponse (
+    @SerializedName("status") val status: Boolean,
+    @SerializedName("message") val message: String,
+    @SerializedName("data") val data: TransactionMetadata,
+    @SerializedName("fees") val fees: String?,
+    @SerializedName("customer") val customer: Customer,
+    @SerializedName("plan") val plan: String?,
+    @SerializedName("requested_amount") val requestedAmount: String
+)
+
+data class TransactionMetadata (
+    @SerializedName("amount") val amount: String,
+    @SerializedName("currency") val currency: String,
+    @SerializedName("status") val transactionStatus: String,
+    @SerializedName("reference") val reference: String,
+    @SerializedName("domain") val domain: String,
+    @SerializedName("gateway_response") val gatewayResponse: String?,
+    @SerializedName("channel") val channel: String?,
+    @SerializedName("ip_address") val ipAddress: String?,
+    @SerializedName("log") val log: TransactionLog
+)
+
+data class TransactionLog(
+    @SerializedName("time_spent") val timeSpent: Int,
+    @SerializedName("attempts") val attempts: Int,
+    @SerializedName("authentication") val authentication: String?,
+    @SerializedName("errors") val errors: Int,
+    @SerializedName("success") val success: Boolean,
+    @SerializedName("channel") val channel: String?,
+    @SerializedName("history") val history: List<TransactionHistory>
+)
+
+data class TransactionHistory(
+    @SerializedName("type") val type: String,
+    @SerializedName("message") val message: String,
+    @SerializedName("time") val time: Int
+)
+
+data class Customer(
+    @SerializedName("id") val id: Int,
+    @SerializedName("customer_code") val customerCode: String,
+    @SerializedName("first_name") val firstName: String?,
+    @SerializedName("last_name") val lastName: String?,
+    @SerializedName("email") val email: String
+)
