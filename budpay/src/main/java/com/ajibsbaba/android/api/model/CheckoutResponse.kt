d18fca5693f78dc99c1c4f771c71ccd85665fdb0
Samuel Ajibade <samuelajibade22@gmail.com>
2023-10-24 12:37:18 +0100

    created class for checkout api response

--- budpay/src/main/java/com/ajibsbaba/android/api/model/CheckoutResponse.kt
+++ budpay/src/main/java/com/ajibsbaba/android/api/model/CheckoutResponse.kt
@@ -1,4 +1,22 @@
 package com.ajibsbaba.android.api.model
 
-class CheckoutResponse {
-}
+import androidx.annotation.Keep
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@Keep
+@JsonClass(generateAdapter = true)
+data class CheckoutResponse (
+    @Json(name = "message")
+    val message: String? = null,
+
+    @Json(name = "reference")
+    val reference: String? = null,
+
+    @Json(name = "status")
+    val status: Boolean? = null,
+
+
+
+
+)
